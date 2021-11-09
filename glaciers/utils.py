9fae7f196eb9f8f3876f44d5ff7c1f2ab2564865
Thandikire Madula <zcapmad@ucl.ac.uk>
2021-11-09 16:53:59 +0000

    added function to check input file format

--- glaciers/utils.py
+++ glaciers/utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import csv
 
 
 def haversine_distance(lat1, lon1, lat2, lon2):
@@ -7,3 +8,22 @@ def haversine_distance(lat1, lon1, lat2, lon2):
     Latitude and longitude for each point are given in degrees.
     """
     raise NotImplementedError
+
+def check_csv(file_path, required_keys):
+
+    '''
+     DOC STRING 
+    '''
+    
+    assert file_path.split(".")[-1] == 'csv', "please input a csv file"
+
+    file_path = Path(file_path)
+
+    with open(file_path, newline='') as file: 
+        read_file = csv.DictReader(file)
+        check_columns = dict(list(read_file)[0])
+
+        for column in required_keys:
+            if column not in check_columns:
+                raise ValueError("csv missing column ", column)
+
