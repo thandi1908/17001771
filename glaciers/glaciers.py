69b165f91bf5878a7744dfc3a3bcbdb6b5a76156
Thandikire Madula <zcapmad@ucl.ac.uk>
2021-11-09 14:50:59 +0000

    Completed read mass balance and add mass balance

--- glaciers/glaciers.py
+++ glaciers/glaciers.py
@@ -13,8 +13,19 @@ class Glacier:
         self.code = code
         self.mass_dict = {}
 
-    def add_mass_balance_measurement(self, year, mass_balance, subregion = True):
-        self.mass_dict[year] = {"mass_balance": mass_balance}
+    def add_mass_balance_measurement(self, year, mass_balance, subregion = False):
+        # does this key exist in the dictionary?
+        if year in self.mass_dict:
+            # is it a sub region measurement
+            if subregion == True and self.mass_dict[year]["subregion"] == True:
+                self.mass_dict[year]["mass_balance"] += mass_balance
+            
+            # if it's not a subregion measurement but the previous was skip it
+            elif subregion == False and self.mass_dict[year]["subregion"] ==True: 
+                pass 
+
+        else: # new year measurement
+            self.mass_dict[year] = {"mass_balance":mass_balance, "subregion": subregion}
 
     def plot_mass_balance(self, output_path):
         raise NotImplementedError
@@ -22,66 +33,59 @@ class Glacier:
         
 class GlacierCollection:
     def __init__(self, file_path):
+
+        # what if the file path isn't a csv? will need to throw as error there 
+        assert file_path.split(".")[-1] == 'csv', "please input a csv file"
+
         file_path = Path(file_path)
         self.file_path = file_path
         self.dict = {}
-        # what if the file path isn't a csv? will need to throw as error there 
         # loading the data from the csv file
         with open(self.file_path, newline='') as file: 
             read_file = csv.reader(file)
             
-            # skip the header of the csv file
+            # skip the header of the csv file [check that the headings are in the expected order...] 
             next(read_file)
             for row in read_file:
                 unit = row[0]
                 name = row[1]
                 idnt = row[2] 
-                lat = row[5]
-                lon = row[6]
+                lat = float(row[5])
+                lon = float(row[6])
                 code = int(row[7]+row[8]+row[9])
-                self.dict[idnt] = [Glacier(idnt,name,unit,lat, lon, code)]
+                self.dict[idnt] = Glacier(idnt,name,unit,lat, lon, code)
         
 
     def read_mass_balance_data(self, file_path):
+        # check that the user has inputed the right file extension 
+        assert file_path.split(".")[-1] == 'csv', "please input a csv file"
+        
         file_path = Path(file_path)
 
         with open(file_path, newline='') as file: 
             csv_reader = csv.reader(file)
 
-            # skip the header o the csv file
+            # skip the header of the csv file [ check that the headings are as expected...]
             next(csv_reader)
-
-            # place holder variables 
-            current_id = 0 
-            current_year = 0 
-            current_mass_balance = 0
-            
             for row in csv_reader:
 
-                if current_id == 0: 
-                    current_id = row[2]
-                    current_year = row[3]
-                    print(row[-3])
-                    current_mass_balance += row[-3]
-                elif row[2] == current_id:
-                    #same glacier different year
-                    if row[3] != current_year:
-                        self.dict[current_id].add_mass_balance_measurement(current_year, current_mass_balance)
-                        current_year =row[3]
-                        current_mass_balance = row[-3]
-                    else:
-                        # check if this is the final value
-                        if row[5] == 9999:
-                            current_mass_balance += 0
-                        else: 
-                            # same glacier same year
-                            current_mass_balance += row[-3]
+                current_id = row[2]
 
+                # is this a submeasurement
+                if int(row[4]) != 9999:
+                    submeasure = True
+                else:
+                    submeasure = False
+                year = row[3]
+
+                # is there a measurement for this year? 
+                if row[-3] == "": 
+                    balance = 0.0 
                 else: 
-                    # different glacier
-                    current_id = row[2]
-                    current_year = row[3]
-                    current_mass_balance = row[-3]
+                    balance = float(row[-3])
+                self.dict[current_id].add_mass_balance_measurement(year, balance, submeasure)
+                
+                
             
 
 
